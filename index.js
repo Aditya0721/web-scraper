500c39667794bf9a9de6d1c05305af8cf03b2fba
Aditya <63107098+Aditya0721@users.noreply.github.com>
2023-01-17 09:55:28 +0530

    web scrap code

---
+++ index.js
@@ -0,0 +1,33 @@
+const axios = require("axios")
+const scrapeit = require("scrape-it")
+const cheerio = require("cheerio")
+
+
+axios.get("https://www.cebm.net/covid-19/in-patients-of-covid-19-what-are-the-symptoms-and-clinical-features-of-mild-and-moderate-case/")
+.then(
+    (res)=>{
+        const $ = cheerio.load(res.data)
+        const data = scrapeit.scrapeHTML($,{scrapedData:{
+            listItem: "table", data: {
+                createdAt: {
+                    selector: ".date"
+                  , convert: x => new Date(x)
+                }
+              , title: "a.article-title"
+              , tags: {
+                    listItem: ".tags > span"
+                }
+              , content: {
+                    selector: "tbody"
+                  , how: "text"
+                }
+              , traverseOtherNode: {
+                    selector: ".upperNode"
+                  , closest: "div"
+                  , convert: x => x.length
+                }
+        }}})
+        console.log(data.scrapedData[0].content.replace('\n', ""))
+    })
+.catch((err)=>{console.log(err)})
+
